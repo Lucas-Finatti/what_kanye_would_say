c42ec5212959bc11dcd466b76949e2d1575d88dd
Finatti <Lucas.finatti@hotmail.com>
2023-09-04 11:42:55 -0300

    [FIX] main text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ButtonComponente from "./services/kanyeWestSaid"
 function App() {
   return (
     <div className="App">
-      
       <header className="header">
         <img src={kanyewestlogo} alt="Kanye West Logo" className="image" />
         <p className="text">What kanye West Wold Say</p>
